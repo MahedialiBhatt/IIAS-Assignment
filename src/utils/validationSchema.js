39b4fc0f2857d184f2dbec29eebfe1d28624a30c
MahedialiBhatt <bhattmahediali92@gmail.com>
2024-02-21 21:20:44 +0530

    improved validations  error messages

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -1,19 +1,37 @@
 const Joi = require("joi");
 
 const checkInSchema = Joi.object({
-  instructor_id: Joi.number().integer().required(),
-  date: Joi.string().isoDate().required(),
+  instructor_id: Joi.number().integer().min(1).required(),
+  date: Joi.string().isoDate().required().messages({
+    "string.base": "Invalid type, date must be a string",
+    "string.isoDate":
+      "Invalid date format, must be in ISO date format (YYYY-MM-DD)",
+    "any.required": "Date is required",
+  }),
   check_in_time: Joi.string()
     .regex(/^([01]\d|2[0-3]):([0-5]\d)$/)
-    .required(),
+    .required()
+    .messages({
+      "string.pattern.base": "Invalid time format, must be in HH:mm format",
+      "any.required": "Check-in time is required",
+    }),
 });
 
 const checkOutSchema = Joi.object({
-  instructor_id: Joi.number().integer().required(),
-  date: Joi.string().isoDate().required(),
+  instructor_id: Joi.number().integer().min(1).required(),
+  date: Joi.string().isoDate().required().messages({
+    "string.base": "Invalid type, date must be a string",
+    "string.isoDate":
+      "Invalid date format, must be in ISO date format (YYYY-MM-DD)",
+    "any.required": "Date is required",
+  }),
   check_out_time: Joi.string()
     .regex(/^([01]\d|2[0-3]):([0-5]\d)$/)
-    .required(),
+    .required()
+    .messages({
+      "string.pattern.base": "Invalid time format, must be in HH:mm format",
+      "any.required": "Check-out time is required",
+    }),
 });
 
 const monthlyReportSchema = Joi.object({
